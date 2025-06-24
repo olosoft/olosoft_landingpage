6c67c3b2b72818ddef8049239e839727752dec5b
olosoft <olosoftvision@gmail.com>
2025-06-24 14:53:59 +0200

    removed testimonials

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import About from "@/components/About";
-import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -12,7 +11,6 @@ const Index = () => {
       <Hero />
       <Features />
       <About />
-      <Testimonials />
       <Contact />
       <Footer />
     </div>
